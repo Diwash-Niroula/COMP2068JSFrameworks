ea6ae70b1b837d0306589ba35d142ab38b95f67a
Diwash-Niroula <156447210+Diwash-Niroula@users.noreply.github.com>
2025-05-29 22:10:42 -0400

    commit 2
    Prompt use for input

--- LAB02/Lab02.js
+++ LAB02/Lab02.js
@@ -3,4 +3,13 @@ const prompt = require('prompt');
 
 // Start the prompt
 prompt.start();
+// Ask user to input ROCK, PAPER, or SCISSORS
+prompt.get(['userSelection'], function (err, result) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Get user input and convert to uppercase to standardize
+  const userSelection = result.userSelection.toUpperCase();
 
